919e08088fe07f3d093a459ee34319aaf823d8c7
wangshaoli <1125550225@qq.com>
2021-05-07 11:17:52 +0800

    实现 点击+1  数字+1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'
 
-ReactDOM.render(
-    <div>hi</div>,
-  document.getElementById('root')
-);
+const root = document.querySelector('#root')
+
+let n = 0
+const App =()=> React.createElement('div', {className: 'red',},
+    [
+        n,
+        React.createElement('button', {
+                onClick: () => {
+                    n++
+                    console.log(n)
+                    ReactDOM.render(App(), root);
+                }
+            },
+            "+1"
+        )
+    ]
+)
+
+ReactDOM.render(App(), root);
